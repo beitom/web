af266a8c454996bac35e62db3cfa6a4a0bfaf7e4
Tom Ginsberg <tom@beit.tech>
2024-10-28 15:40:44 -0700

    updated header

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { SiteFooter } from "@/components/site-footer"
-import { SiteHeader } from "@/components/site-header"
+import { SiteHeader } from "@/components/site-header-xyz"
 import React from "react"
 
 interface MarketingLayoutProps {
