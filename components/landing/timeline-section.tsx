8bf44631f12130cc50a6e8f145edaf5f7518696f
Tom Ginsberg <tom@beit.tech>
2024-10-29 12:21:04 -0700

    added content

--- components/landing/timeline-section.tsx
+++ components/landing/timeline-section.tsx
@@ -88,9 +88,20 @@ export function BeitTimeline() {
             <Hyperlink
               link="https://www.linkedin.com/pulse/beit-inc-raises-14m-seed-funding-paulina-mazurek/"
               text=" $1.4 million USD "
-            />
-            led by <Hyperlink link="https://kindredcapital.vc/" text=" Kindred Capital " />, enabling the expansion of
-            our research and development efforts in quantum computing technologies.
+            />{" "}
+            consortium led by <Hyperlink link="https://kindredcapital.vc/" text=" Kindred Capital " />, including{" "}
+            <Hyperlink link={"https://mantaray.vc/"} text=" Manta Ray Ventures" />,{" "}
+            <Hyperlink link="http://www.fireboltventures.com/" text=" Firebolt Ventures" />,
+            <Hyperlink
+              link="https://www.linkedin.com/in/charlessonghurst?lipi=urn%3Ali%3Apage%3Ad_flagship3_pulse_read%3B4HlsDz%2BKR3yFaEOIgA%2FnNg%3D%3D"
+              text=" Charles M. Songhurst "
+            />{" "}
+            and <Hyperlink link="http://linkedin.com/in/george-armoyan-87a9a4110" text=" George Armoyan" />. Our new
+            investors are our CDL investors: <Hyperlink link="https://dcvc.com/" text=" Data Collective VC" />,{" "}
+            <Hyperlink link="https://www.bloomberg.com/lp/beta/" text=" Bloomberg Beta " /> and{" "}
+            <Hyperlink link="https://www.s28capital.com/" text="Spectrum28" />. The funds will be used to match the
+            company’s EU grants of $2.6M USD in order to speed up the development of quantum algorithms, as well as to
+            expand business development.
           </p>
         </div>
       )
@@ -100,13 +111,29 @@ export function BeitTimeline() {
       date: "Oct 2020",
       content: (
         <div>
-          <p className="text-neutral-800 dark:text-neutral-200 font-normal mb-8">
+          <p className="text-neutral-800 dark:text-neutral-200 font-lg mb-4">
             Achieved a <Hyperlink link="https://beit.tech/blog/honeywell-h0.html" text=" significant breakthrough " />{" "}
             in quantum advantage for unstructured search in high-dimensional spaces in partnership with the Honeywell
             Quantum Solutions team (now <Hyperlink link="https://www.quantinuum.com/" text=" Quantinuum" />
             ). This work showcased our Micro Diffuser algorithm's potential in harnessing quantum capabilities beyond
             traditional constraints.
           </p>
+
+          <figure className="pl-4 mb-3 border p-3 rounded-lg">
+            <blockquote className="text-lg italic font-semibold text-white">
+              <p>
+                " We look to enable users with the most capable quantum computing environment. We’re thrilled to see how
+                BEIT has used our systems in ground breaking demonstrations of Grover’s algorithm and how they have
+                leveraged Honeywell’s technology differentiators."
+              </p>
+            </blockquote>
+            <figcaption className="flex items-center mt-3 space-x-3 rtl:space-x-reverse">
+              <div className="flex items-center divide-x-2 rtl:divide-x-reverse divide-gray-700">
+                <cite className="pr-3 font-xs text-white">- Tony Uttley</cite>
+                <span className="ps-2">(President of Honeywell Quantum Solutions)</span>
+              </div>
+            </figcaption>
+          </figure>
         </div>
       )
     },
